fc2829b991b911fa189e88e270254ec9e58a750d
mohamed-mahsoub <Mohamed.mahsoub@uranium-corp.com>
2024-03-22 14:01:22 +0200

    feat: fetch the list of departments in departments page with SWR to cache the request

--- src/api/endPoints.js
+++ src/api/endPoints.js
@@ -2,4 +2,7 @@ export const ENDPOINTS = {
   auth: {
     userLogin: "/token",
   },
+  departments: {
+    getAllDepartments: "/departments",
+  },
 };
--- src/pages/departments.page.jsx
+++ src/pages/departments.page.jsx
@@ -1,4 +1,20 @@
+import { ENDPOINTS } from "@/api/endPoints";
+import { dynamicAxiosRequest } from "@/helpers/utilities/dynamicAxiosRequest";
+import useSWR from "swr";
+
 const Departments = () => {
+  //fetch data by SWR to easy handle fetching and caching data if there isn't any changes to avoid unnessary requests
+  const fetcher = (endPoint) =>
+    dynamicAxiosRequest({
+      baseUrl: import.meta.env.VITE_BASE_URLL,
+      endPoint,
+    }).then((res) => res);
+  const {
+    data: departments,
+    isLoading,
+    error,
+  } = useSWR(`${ENDPOINTS.departments.getAllDepartments}`, fetcher);
+  console.log("departments", departments);
   return (
     <div className="h-screen flex justify-center items-center">
       Departments page
